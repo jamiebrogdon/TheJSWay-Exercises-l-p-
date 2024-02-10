ad513cdac19b799df98baae897fabd7e2e5a3f34
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-10 11:34:42 -0600

    Add files via upload

--- TheJSWay.js
+++ TheJSWay.js
@@ -110,6 +110,136 @@ if (number1 === number2){
     }
 //is there more I could practice with? Might come back to this! :) 
 
+//CHAPTER 3
+//Take a look at the following program: 
+//Before executing it, try to guess the final values of variables nb1, nb2 and nb3 depending on their initial values. 
+//Complete the following table (will be below the following problem)
+
+let nb1 = Number(prompt("Enter nb1:"));
+let nb2 = Number(prompt("Enter nb2:"));
+let nb3 = Number(prompt("Enter nb3:"));
+
+if (nb1 > nb2) {
+  nb1 = nb3 * 2;
+} else {
+  nb1++;
+  if (nb2 > nb3) {
+    nb1 += nb3 * 3;
+  } else {
+    nb1 = 0;
+    nb3 = nb3 * 2 + nb2;
+  }
+}
+console.log(nb1, nb2, nb3);
+
+//table to problem above:
+
+nb1 = nb2 = nb3 = 4	//nb1 final value = 0,  nb2 final value = 4,  nb3 final value = 12
+
+nb1 = 4, nb2 = 3, nb3 = 2 //nb1 final value = 4,  nb2 final value = 3,  nb3 final value = 2
+
+nb1 = 2, nb2 = 4, nb3 = 0 //nb1 final value = 3,  nb2 final value = 4,  nb3 final value = 0
+
+//CHAPTER 3
+//Write a program that accepts a month number (between 1 and 12), then shows the number of days of that month. Leap years are excluded. Incorrect inputs must be taken into account.
+
+const monthNumber = Number(prompt("Enter Month Number 1-12:"));
+//first attempted using parentheses in the case, and learned that it will read undefined since its converted to a number not a string!
+switch (monthNumber) {
+    case 1: //January
+        console.log("this month has 31 days!");
+        break;
+    case 2: //February
+        console.log("this month has 28 days!");
+        break;
+    case 3: //March
+        console.log("this month has 31 days!");
+        break;
+    case 4: //April
+        console.log("this month has 30 days!");
+        break;
+    case 5: //May
+        console.log("this month has 31 days!");
+        break;
+    case 6: //June
+        console.log("this month has 30 days!");
+        break;
+    case 7: //July
+        console.log("this month has 31 days!");
+        break;
+    case 8: //August
+        console.log("this month has 30 days!");
+        break;
+    case 9: //September
+        console.log("this month has 31 days!");
+        break;
+    case 10: //October
+        console.log("this month has 30 days!");
+        break;
+    case 11: //November
+        console.log("this month has 31 days!");
+        break;
+    case 12: //December
+        console.log("this month has 30 days!");
+        break;
+    default: 
+        console.log("This is not a number that matches with a month!")
+}   
+
+//The switch above feels a little 'long' and more lines of code than it might need.
+//Starting with creating a variable so the user can input the month number
+//February is the only month that doesn't follow the rules on number of days, so that's gotta be the first thing to check
+//Knowing that I can find an even number by using a modulus - and that all even months are 30 days and all odd months are 31
+
+const numberMonth = Number(prompt("Enter Month Number 1-12:"));
+
+if (numberMonth === 2){
+    console.log("This month has 28 days!");
+    }else if (numberMonth % 2 ===0){
+        console.log("This month has 30 days!");
+        }else{
+            console.log("This month has 31 days!");
+}
+
+//CHAPTER 3
+//Write a program that asks for a time under the form of three information (hours, minutes, seconds). 
+//The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
+//COMING BACK TO THIS ----> NOT COMPLETE
+/*
+14h17m59s should give 14h18m0s
+6h59m59s should give 7h0m0s
+23h59m59s should give 0h0m0s (midnight)
+*/
+/*
+let hour = Number(prompt("what hour is it?"));
+let minute = Number(prompt("what minute is it?"));
+let second = Number(prompt("what second is it?"));
+
+if (second > 59 || second < 0 || minute > 59 || minute < 0 || hour > 23 || hour < 0) 
+{
+    console.log("please, enter a valid time");
+} else if ((hour === 23) && (minute === 59) && (second === 59)) {
+    hour = 0;
+    minute = 0;
+    second = 0;
+    console.log(`${hour}h ${minute}m ${second}s`);
+
+} else if ((second <= 59) && (minute <= 59) && (hour <= 23)) {
+    hour++;
+    minute =0;
+    second =0;
+    console.log(`${hour}h ${minute}m ${second}s`);
 
 
+} else if (second < 59 && hour <= 23 && minute <= 59) {
+    second++;
+    console.log(`${hour}h ${minute}m ${second}s`);
 
+}
+
+else if (second <= 59) {
+    second = 0;
+    minute++;
+    console.log(`${hour}h ${minute}m ${second}s`);
+} 
+*/
